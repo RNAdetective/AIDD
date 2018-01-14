f3ead78f86450445b4a30a97c64d607c8ae0fb73
nplonsk2 <nplonsk2@kent.edu>
2018-01-13 22:27:36 -0500

    Update set_upCode.sh

--- set_upCode.sh
+++ set_upCode.sh
@@ -3,39 +3,39 @@
 sudo adduser user vboxsf
 ##this updates java for the picard tool
 sudo apt-add-repository ppa:openjdk-r/ppa
-sudo apt-get -y update
-sudo apt-get -y install openjdk-8-jdk
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install openjdk-8-jdk
 sudo update-alternatives --config java
 ##this installs curl and other attachments for adding R packages later
-sudo apt-get -y install curl
-sudo service apache2 restart
-sudo apt-get -y install php5-curl
-sudo service apache2 restart
-sudo apt-get -y install aptitude
-sudo apt-get -y install libcurl4-openssl-dev
-sudo apt-get -y install libxml2-dev
-sudo apt-get -y install libssl-dev
-sudo apt-get -y install libgsl0ldbl
-sudo apt-get -y install gsl-bin libgsl0-dev
-sudo apt-get -y install gsl-bin libgsl2
-sudo apt-get -y install libtiff-dev
-sudo apt-get -y install build-essential python2.7-dev python-htseq
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes upgrade
+sudo apt-get --yes --force-yes install curl
+sudo apt-get --yes --force-yes install php5-curl
+sudo apt-get --yes --force-yes install aptitude
+sudo apt-get --yes --force-yes install libcurl4-openssl-dev
+sudo apt-get --yes --force-yes install libxml2-dev
+sudo apt-get --yes --force-yes install libssl-dev
+sudo apt-get --yes --force-yes install libgsl0ldbl
+sudo apt-get --yes --force-yes install gsl-bin libgsl0-dev
+sudo apt-get --yes --force-yes install gsl-bin libgsl2
+sudo apt-get --yes --force-yes install libtiff-dev
+sudo apt-get --yes --force-yes install build-essential python2.7-dev python-htseq
 ## this uninstalls old version of R that came with biolinux8
-sudo apt-get -y remove r-base-core
+sudo apt-get --yes --force-yes remove r-base-core
 ## the following adds permission and downloads the newest version of R sets R up for R package downloads required for analysis.
 sudo sh -c 'echo' "deb http://cran.rstudio.com/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo add-apt-repository ppa:marutter/rdev
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install r-base
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes upgrade
+sudo apt-get --yes --force-yes install r-base
 ## this downloads the tool that allows R to find and downloads certain packages needed for analysis.
-sudo apt-get -y install r-cran-rmysql
+sudo apt-get --yes --force-yes install r-cran-rmysql
 mkdir /home/user/Pipelines
 mkdir /home/user/Pipelines/HISAT2_pipeline
 mkdir /home/user/Pipelines/HISAT2_pipeline/bin
 ##sratoolkit
-wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz -O sratoolkit.tar.gz
 tar -vxzf /home/user/sratoolkit.tar.gz
 rm /home/user/sratoolkit.tar.gz
 cp /home/user/sratoolkit.2.8.2-1-ubuntu64/bin/* /home/user/Pipelines/HISAT2_pipeline/bin
@@ -48,7 +48,7 @@ rm /home/user/samtools.tar.bz2
 cd /home/user/samtools-1.3.1
 make
 sudo make prefix=/usr/local/bin install
-cp /home/user/samtools-1.3.1/samtools /home/user/Pipeline/HISAT2_pipeline/bin
+cp /home/user/samtools-1.3.1/samtools /home/user/Pipelines/HISAT2_pipeline/bin
 cd /home/user/
 mv /home/user/samtools-1.3.1 /home/user/Pipelines/HISAT2_pipeline
 ##install HISAT2
@@ -77,8 +77,6 @@ mv /home/user/cufflinks-2.2.1.Linux_x86_64 /home/user/Pipelines/HISAT2_pipeline
 wget https://github.com/alexdobin/STAR/archive/2.5.3a.tar.gz
 tar -xzf 2.5.3a.tar.gz
 rm /home/user/2.5.3a.tar.gz
-cd /home/user/STAR-2.5.3a
-/
 cp /home/user/STAR-2.5.3a/bin/Linux_x86_64/STAR /home/user/Pipelines/HISAT2_pipeline/bin
 mv /home/user/STAR-2.5.3a /home/user/Pipelines/HISAT2_pipeline
 ##picard tool
