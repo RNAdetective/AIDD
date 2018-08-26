a341642a99e45f5e755204f9e69844d48dde5b76
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-08-25 20:34:38 -0400

    Delete createDEtables.R

--- Rscripts/createDEtables.R
+++
@@ -1,3 +0,0 @@
-## still need to create the .csv files for the topGO analysis.  These need to be top100 DE, all down above FC=1 and all up reg same FC=2.  one set for gene and one set for transcript.
-
-
