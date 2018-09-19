de6294df6656bf42fc994c153a085f5e88c85c11
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:10:57 -0400

    Delete editgenetransvenn.R

--- Rscripts/editgenetransvenn.R
+++
@@ -1,6 +0,0 @@
-table1 <- read.csv("/media/sf_AIDD/Results/DESeq2/gene/differential_expression/venndiagrams/regulationidGlistclasscell_lineadded.csv")
-colnames(table1)[3] <- "regulationcell_lineclassranscript"
-colnames(table1)[2] <- "XX"
-table1$XX <- NULL
-write.csv(table1, "/media/sf_AIDD/Results/DESeq2/gene/differential_expression/venndiagrams/regulationidGlistclasscell_lineadded.csv", row.names=FALSE)
-
