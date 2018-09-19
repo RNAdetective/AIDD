7f65436efda20e2492b374ff7282ef0045ba58ba
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:11:46 -0400

    Delete transcriptcombine.R

--- Rscripts/transcriptcombine.R
+++
@@ -1,6 +0,0 @@
-multmerge = function(mypath){
-filenames=list.files(path=mypath, full.names=TRUE)
-datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
-Reduce(function(x,y) {merge(x,y)}, datalist)}
-mymergeddata = multmerge("/media/sf_AIDD/Results/DESeq2/transcript/differential_expression/Listnamesfinalresults/final/gene/merged/")
-write.csv(mymergeddata, "/media/sf_AIDD/Results/DESeq2/transcript/differential_expression/Listnamesfinalresults/final/genefinal.csv", row.names=FALSE)
