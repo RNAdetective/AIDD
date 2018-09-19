29ec9afedce7f787b5063499884df51705a1bdb7
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:28:29 -0400

    Delete splitfq2.py

--- extra/splitfq2.py
+++
@@ -1,9 +0,0 @@
-fastq_1 = open('fastq_r1.fastq')
-
-fastq_2 = open('fastq_r2.fastq')
-
-[r1.write(line) if (i % 8 < 4) else r2.write(line) for i, line in enumerate(open('test.fastq'))]
-
-fastq_1.close()
-
-fastq_2.close()
