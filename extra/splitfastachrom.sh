35322da8e17b5830998acb45d198f4862419bf7c
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:28:17 -0400

    Delete splitfastachrom.sh

--- extra/splitfastachrom.sh
+++
@@ -1,8 +0,0 @@
-#https://crashcourse.housegordon.org/split-fasta-files.html
-mkdir directory
-cd directory
-csplit -s -z /media/sf_sim/directory.txt '/>/' '{*}'
-for i in xx* ; do \
-  n=$(sed 's/$// ; s/ .*// ; 1q' "$i") ; \
-  mv "$i" "$n.txt" ; \
- done
