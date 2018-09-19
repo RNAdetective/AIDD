96e64d4bcde960878ee32370fea5df0fd5a57c76
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:11:51 -0400

    Delete transpose.R

--- Rscripts/transpose.R
+++
@@ -1,3 +0,0 @@
-table1 <- read.csv("/media/sf_AIDD/Results/variant_calling/haplotype/gene/amino_acid/globalrun_namesubs.csv", row.names=1)
-table2 <- t(table1)
-write.csv(table2, "/media/sf_AIDD/Results/variant_calling/haplotype/gene/amino_acid/globalrun_namesubsfinal.csv")
