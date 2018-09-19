8934080e50c66b0d008277337789825d4c0ce514
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:12:05 -0400

    Delete variantsubsfinalmerge.R

--- Rscripts/variantsubsfinalmerge.R
+++
@@ -1,7 +0,0 @@
-multmerge = function(mypath){
-filenames=list.files(path=mypath, full.names=TRUE)
-datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
-Reduce(function(x,y) {merge(x,y)}, datalist)}
-mymergeddata = multmerge("/media/sf_AIDD/Results/variant_calling/haplotype/gene/nucleotide/allfinal/merged/")
-write.csv(mymergeddata, "/media/sf_AIDD/Results/variant_calling/haplotype/gene/nucleotide/allfinal/ntsubsallconfinal.csv", row.names=FALSE)
-
