34bbbcf7cceb8dfc0921d0d74aacbba3a75f3d45
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:28:40 -0400

    Delete test2.sh

--- extra/test2.sh
+++
@@ -1,8 +0,0 @@
-R
-library(ggplot2)
-table1 <- read.csv("/media/sf_AIDD/Results/ADAR150RG.csv")
-jpeg("/media/sf_AIDD/Results/ADAR150RG.jpeg")
-ggplot(table1, aes(x=QR, y=B1_004)) + geom_point() + geom_smooth(method=lm) + labs(x="QR", y = "B1_004") + theme_classic()
-dev.off()
-cor.test( ~ B1_004 + QR, data=table1, method = "pearson", continuity = FALSE, conf.level = 0.95)
-
