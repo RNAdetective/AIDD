b7376aaadb55b18bb23804b16d4f945a9854ba4f
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-09-19 01:16:03 -0400

    Delete splitfastachrom2.sh

--- bashScripts/variantcalling/splitfastachrom2.sh
+++
@@ -1,9 +0,0 @@
-#https://crashcourse.housegordon.org/split-fasta-files.html
-mkdir /media/sf_AIDD/Results/variant_calling/haplotype/gene/vcf_tables/globalrun_name/
-cd /media/sf_AIDD/Results/variant_calling/haplotype/gene/vcf_tables/globalrun_name/
-csplit -s -z /media/sf_AIDD/raw_data/snpEff/globalrun_name.csv '/>/' '{*}'
-for i in xx* ; do \
-  n=$(sed 's/>// ; s/ .*// ; 1q' "$i") ; \
-  mv "$i" "$n.csv" ; \
- done
-cd /home/user/
