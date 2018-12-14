d65ed42ddaed888d8fd67bd13ba20413f7b04295
Noel-Marie Plonski <nplonsk2@kent.edu>
2018-12-14 12:50:24 -0500

    Delete splitfastachrom.sh

--- ExToolset/splitfastachrom.sh
+++
@@ -1,9 +0,0 @@
-#https://crashcourse.housegordon.org/split-fasta-files.html
-mkdir /media/sf_AIDD/Results/DESeq2/gene/differential_expression/venndiagrams/directory
-cd /media/sf_AIDD/Results/DESeq2/gene/differential_expression/venndiagrams/directory
-csplit -s -z /media/sf_AIDD/Results/DESeq2/gene/differential_expression/venndiagrams/directory.csv '/>/' '{*}'
-for i in xx* ; do \
-  n=$(sed 's/>// ; s/ .*// ; 1d ; 1q' "$i") ; \
-  mv "$i" "$n.csv" ; \
-  sed -i 's/ "$n.csv" 
- done
