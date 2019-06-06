831c4b3d1fd807bb19c680e2b60a2a20a2240ca9
Noel-Marie Plonski <nplonsk2@kent.edu>
2019-06-05 22:10:59 -0400

    Add files via upload

--- updateAIDDscripts.sh
+++ updateAIDDscripts.sh
@@ -3,5 +3,10 @@ rm -r /home/user/AIDD/AIDD
 wget https://github.com/RNAdetective/AIDD/raw/master/AIDDscripts.tar.gz #this will get pipeline scripts
 tar -vxzf AIDDscripts.tar.gz #uncompress them
 rm AIDDscripts.tar.gz #remove compressed file
+mkdir /home/user/AIDD/AIDD
 cp -r /home/user/AIDDscripts/* /home/user/AIDD/AIDD/ #moves all the updated scripts to the correct directory
+AIDDs=/home/user/AIDD/AIDD
+gsettings set org.gnome.desktop.background picture-uri "file://"$AIDDs"/AIDDlogo.jpg"
+rm -r /home/user/AIDDscripts
+
 
