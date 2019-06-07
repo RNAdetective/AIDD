47da0a2f705cace15a3acf8ac35ae46895ca2ff1
Noel-Marie Plonski <nplonsk2@kent.edu>
2019-06-06 21:11:57 -0400

    Add files via upload

--- upadateAIDDRef.sh
+++ upadateAIDDRef.sh
@@ -11,14 +11,16 @@ fi
 }
 organize_ref() {
 perl -e 'use File::Temp qw/tempdir/; use IO::File; $d=tempdir; $fh; map{if(m/^\>(\S+)\s/){$fh=IO::File->new(">$d/$1.fa");} print $fh $_;}`cat ref1.fa`; foreach $c(1..22,X,Y,MT){print `cat $d/$c.fa`}; print `cat $d/GL*`' > ref2.fa
-java -jar "$home_dir"/AIDD/AIDD_tools/picard.jar CreateSequenceDictionary REFERENCE="$ref_dir_path"/ref2.fa OUTPUT="$ref_dir_path"/ref2.dict
+java -jar "$javaset" /home/user/AIDD/AIDD_tools/picard.jar CreateSequenceDictionary REFERENCE="$ref_dir_path"/ref2.fa OUTPUT="$ref_dir_path"/ref2.dict
 samtools faidx "$ref_dir_path"/ref2.fa
 }
-ref_dir_path=/home/user/AIDD/references
+javaset="-Xmx20G -XX:-UseGCOverheadLimit -XX:ParallelGCThreads=2 -XX:ReservedCodeCacheSize=1024M -Djava.io.tmpdir="$dir_path"/tmp"; # sets java tools
+ref_dir_path=/media/sf_AIDD/references
+mkdir "$ref_dir_path"
 cd "$ref_dir_path"
 for files in ref1.fa ref2.fa ref2.dict ref2.fa.fai dbsnp.vcf dbsnp.vcf.idx ;
 do
-  rm "$files"
+  rm /home/user/AIDD/references/"$files"
 done
 ftpsite=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz # ref1.fa
 ref_name=ref1.fa
@@ -27,3 +29,7 @@ ftpsite=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/dbsnp_138.b3
 ref_name=dbsnp.vcf
 downloaded_ref
 organize_ref
+for files in ref1.fa ref2.fa ref2.dict ref2.fa.fai dbsnp.vcf dbsnp.vcf.idx ;
+do
+  mv "$ref_dir_path"/"$files" /home/user/AIDD/references
+done
