b8465a1ce40e4b771472e4c2832329a54d44968a
Noel-Marie Plonski <nplonsk2@kent.edu>
2019-09-15 17:18:51 -0400

    Update set_up.sh

--- Instancesetup/set_up.sh
+++ Instancesetup/set_up.sh
@@ -89,5 +89,6 @@ toolz
 tool=hisat2-2.1.0-Linux_x86_64 #install HISAT2
 toolz
 cp "$tool_dir"/hisat2-2.1.0/* "$tool_dir_bin"
-gsettings set org.gnome.desktop.background picture-uri "file://"$home"/AIDD/AIDDlogo.jpg"
+gsettings set org.gnome.desktop.background picture-uri "file://"$home"/AIDD/AIDD/AIDDlogo.jpg"
 cd
+mv "$home_dir"/AIDD/Desktop/* "$home_dir"/Desktop
