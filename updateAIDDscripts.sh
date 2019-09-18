71a5a8ceec68678a98f50789afe31072e65784e4
Noel-Marie Plonski <nplonsk2@kent.edu>
2019-09-18 01:25:20 -0400

    Add files via upload

--- updateAIDDscripts.sh
+++ updateAIDDscripts.sh
@@ -1,11 +1,19 @@
-mv /home/user/AIDD/AIDD_tools/snpEff/snpEff.jar /home/user/AIDD/AIDD_tools/snpEff.jar
-mv /home/user/AIDD/AIDD_tools/snpEff/snpEff.jar /home/user/AIDD/AIDD_tools/snpEff.config
-rm -r /home/user/AIDD/AIDD
-wget https://github.com/RNAdetective/AIDD/raw/master/AIDDscripts.tar.gz #this will get pipeline scripts
-tar -vxzf AIDDscripts.tar.gz #uncompress them
-rm AIDDscripts.tar.gz #remove compressed file
-mkdir /home/user/AIDD/AIDD
-cp -r /home/user/AIDDscripts/* /home/user/AIDD/AIDD/ #moves all the updated scripts to the correct directory
-AIDDs=/home/user/AIDD/AIDD
-gsettings set org.gnome.desktop.background picture-uri "file://"$AIDDs"/AIDDlogo.jpg"
-rm -r /home/user/AIDDscripts
+#!/usr/bin/env bash
+home_dir="$1"
+echo "Do you want to update AIDD scripts? yes or no"
+read answer
+if [ "$answer" == "yes" ];
+then
+  echo "Now updating AIDD scripts"
+  #rm -r "$home_dir"/AIDD/AIDD
+  wget https://github.com/RNAdetective/AIDD/raw/master/AIDDscripts.tar.gz #this will get pipeline scripts
+  #tar -vxzf AIDDscripts.tar.gz #uncompress them
+  #rm AIDDscripts.tar.gz #remove compressed file
+  #mkdir "$home_dir"/AIDD/AIDD
+  #cp -r "$home_dir"/AIDDscripts/* /home/user/AIDD/AIDD/ #moves all the updated scripts to the correct directory
+  #AIDDs="$home_dir"/AIDD/AIDD
+  #gsettings set org.gnome.desktop.background picture-uri "file://"$AIDDs"/AIDDlogo.jpg"
+  #rm -r "$home_dir"/AIDDscripts
+else
+  echo "Not updating AIDD scripts and leaving current AIDD scripts as is."
+fi
