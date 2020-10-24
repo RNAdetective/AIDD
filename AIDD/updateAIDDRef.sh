208b4e42c015b1dd8554b86d6c2825a1995066eb
Noel-Marie Plonski <nplonsk2@kent.edu>
2020-10-24 19:35:43 -0400

    Add files via upload

---
+++ AIDD/updateAIDDRef.sh
@@ -0,0 +1,55 @@
+downloaded_ref() {
+wget "$ftpsite" -O "$ref_name".gz
+gunzip "$ref_name".gz
+if [ -s "$ref_dir_path"/"$ref_name" ] ;
+then
+  rm "$ref_name".gz
+else
+  echo "_________________________________________________________
+Can't find "$ref_name" file"
+fi
+}
+organize_ref() {
+perl -e 'use File::Temp qw/tempdir/; use IO::File; $d=tempdir; $fh; map{if(m/^\>(\S+)\s/){$fh=IO::File->new(">$d/$1.fa");} print $fh $_;}`cat ref1.fa`; foreach $c(1..22,X,Y,MT){print `cat $d/$c.fa`}; print `cat $d/GL*`' > ref2.fa
+java -jar -Djava.io.tmpdir=/media/sf_AIDD/tmp /home/user/AIDD/AIDD_tools/picard.jar CreateSequenceDictionary REFERENCE="$ref_dir_path"/ref2.fa OUTPUT="$ref_dir_path"/ref2.dict
+samtools faidx "$ref_dir_path"/ref2.fa
+}
+createdir() {
+if [ ! -d "$dirtomake" ];
+then
+  mkdir "$dirtomake"
+fi
+}
+ref_dir_path=/media/sf_AIDD/references
+if [ -d "$ref_dir_path" ];
+then
+  rm -r "$ref_dir_path"
+  dirtomake="$ref_dir_path"
+  createdir
+else
+  dirtomake="$ref_dir_path"
+  createdir
+fi
+cd "$ref_dir_path"
+for files in ref1.fa ref2.fa ref2.dict ref2.fa.fai dbsnp.vcf dbsnp.vcf.idx ;
+do
+  refile=/home/user/AIDD/references/"$files"
+  if [ -f "$refile" ];
+  then
+    rm "$refile"
+  fi
+done
+ftpsite=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz # ref1.fa
+ref_name=ref1.fa
+downloaded_ref
+organize_ref
+ftpsite=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/dbsnp_138.b37.vcf.gz
+ref_name=dbsnp.vcf
+downloaded_ref
+for files in ref1.fa ref2.fa ref2.dict ref2.fa.fai dbsnp.vcf dbsnp.vcf.idx ;
+do
+  if [ -f "$ref_dir_path"/"$files" ];
+  then
+    mv "$ref_dir_path"/"$files" /home/user/AIDD/references
+  fi
+done
---
+++ AIDD/updateAIDDscripts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+home_dir="$1"
+echo "Do you want to update AIDD scripts? yes or no"
+read answer
+if [ "$answer" == "yes" ];
+then
+  echo "Now updating AIDD scripts"
+  rm -r "$home_dir"/AIDD/AIDD
+  wget https://github.com/RNAdetective/AIDD/raw/master/AIDDscripts.tar.gz #this will get pipeline scripts
+  tar -vxzf AIDDscripts.tar.gz #uncompress them
+  rm AIDDscripts.tar.gz #remove compressed file
+  mkdir "$home_dir"/AIDD/AIDD
+  cp -r "$home_dir"/AIDDscripts/* "$home_dir"/AIDD/AIDD/ #moves all the updated scripts to the correct directory
+  AIDDs="$home_dir"/AIDD
+  gsettings set org.gnome.desktop.background picture-uri "file://"$AIDDs"/AIDDlogo.jpg"
+  rm -r "$home_dir"/AIDDscripts
+else
+  echo "Not updating AIDD scripts and leaving current AIDD scripts as is."
+fi
