a333bcc76e2042f7bbf19d27eacd2e54caadd7bd
Noel-Marie Plonski <nplonsk2@kent.edu>
2020-12-28 15:21:24 -0500

    Delete extra.sh

--- AIDD/ExToolset/scripts/extra/extra.sh
+++
@@ -1,3 +0,0 @@
-file_in=/media/sf_AIDD/MDD/Results/guttman/excitomefreq_count_matrixANOVAnew.csv
-header=$(cat "$file_in" | head -n 1)
-cat "$file_in" | sed 1d | nawk -F',' '{for(i=1;i<=NF;i++) $i=($i>0) ? 1 : $i}1' | sed 's/ /,/g' | sed 's/ /0/g' | sed '1i '$header'' >> /media/sf_AIDD/MDD/Results/guttman/guttediting_count_matrixANOVAnew.csv
